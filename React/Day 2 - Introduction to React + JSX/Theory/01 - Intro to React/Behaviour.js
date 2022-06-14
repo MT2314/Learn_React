ec581febbb4b43c241e40efc72f05566616c89d9
Mike Tkachuk <mtkachuk@tvo.org>
2022-06-14 13:29:00 -0400

    Day 2 training

---
+++ React/Day 2 - Introduction to React + JSX/Theory/01 - Intro to React/Behaviour.js
@@ -0,0 +1,10 @@
+// ? Function Hello => Returns "Hello World"
+function Hello() {
+  return <h1>Hello World!</h1>;
+}
+function Goodbye() {
+  return <h1>Goodbye World!</h1>;
+}
+// ? ReactDOM.render() is a fancy React function that that our React elements/components
+// ? and renders them on the page
+ReactDOM.render([<Hello />, <Goodbye />], document.getElementById("myDiv"));
